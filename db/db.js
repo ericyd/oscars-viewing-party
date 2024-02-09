92c7dc5d7e1eac27a36955c3b230e550774b1895
Eric Dauenhauer <eric@ericyd.com>
2024-02-08 21:09:07 -0600

    remove log

--- db/db.js
+++ db/db.js
@@ -2,8 +2,6 @@ import dotenv from 'dotenv'
 dotenv.config()
 import knex from 'knex'
 
-console.log(process.env.DB_CONNECTION_STRING)
-
 export const db = knex({
   client: 'pg',
   connection: process.env.DB_CONNECTION_STRING,
