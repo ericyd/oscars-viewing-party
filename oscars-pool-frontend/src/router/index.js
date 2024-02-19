8418fa12d7f5f6ece0f1a9540b1ef7201cc514a3
Eric Dauenhauer <eric@ericyd.com>
2024-02-19 16:59:37 -0600

    update router

--- oscars-pool-frontend/src/router/index.js
+++ oscars-pool-frontend/src/router/index.js
@@ -10,12 +10,22 @@ const router = createRouter({
       component: HomeView
     },
     {
-      path: '/about',
-      name: 'about',
+      path: '/room/:roomId',
+      name: 'room',
       // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
+      // this generates a separate chunk (Room.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      component: () => import('../views/RoomView.vue')
+    },
+    {
+      path: '/room/:roomId/person/:userId',
+      name: 'person',
+      component: () => import('../views/PersonView.vue')
+    },
+    {
+      path: '/predictions/:year',
+      name: 'predictions',
+      component: () => import('../views/PredictionView.vue')
     }
   ]
 })
