05f8e94d090ba8f808f28d649fb91cd4438f928e
Eric Dauenhauer <eric@ericyd.com>
2024-02-24 10:12:29 -0600

    vue formatting

--- oscars-pool-frontend/src/router/index.js
+++ oscars-pool-frontend/src/router/index.js
@@ -1,5 +1,5 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from 'vue-router';
+import HomeView from '../views/HomeView.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,27 +7,27 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView
+      component: HomeView,
     },
     {
-      path: '/room/:roomId',
+      path: '/:roomId',
       name: 'room',
       // route level code-splitting
       // this generates a separate chunk (Room.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/RoomView.vue')
+      component: () => import('../views/RoomView.vue'),
     },
     {
-      path: '/room/:roomId/person/:userId',
+      path: '/:roomId/person/:userId',
       name: 'person',
-      component: () => import('../views/PersonView.vue')
+      component: () => import('../views/PersonView.vue'),
     },
     {
       path: '/predictions/:year',
       name: 'predictions',
-      component: () => import('../views/PredictionView.vue')
-    }
-  ]
-})
+      component: () => import('../views/PredictionView.vue'),
+    },
+  ],
+});
 
-export default router
+export default router;
